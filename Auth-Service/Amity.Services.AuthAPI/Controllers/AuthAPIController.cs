c507d3b76927997894f293e63c67367964e766b4
naozumi23 <naozumi1024@outlook.jp>
2024-07-28 11:36:17 +0900

    auth-main initial commit

---
+++ Auth-Service/Amity.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,49 @@
+﻿using Amity.Services.AuthAPI.Models.Dto;
+using Amity.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amity.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private ResponseDto _response;
+
+        public AuthAPIController(IAuthService authService)
+        {
+            _authService = authService;
+            _response = new ResponseDto();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
+        {
+
+            var errorMessage = await _authService.Register(model);
+            if (!string.IsNullOrEmpty(errorMessage)) { 
+                _response.IsSuccess = true;
+                _response.Message = errorMessage;
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            var loginResponse = await _authService.Login(model);
+            if(loginResponse.User == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "ユーザ名またはパスワードが正しくありません";
+                return BadRequest(_response);
+            }
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+    }
+}
